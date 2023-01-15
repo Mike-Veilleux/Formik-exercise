9c255cb281aba039fa6924b785df5b8819575da6
Michael Veilleux <>
2023-01-15 15:36:41 +0800

    refactor and small updates

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Formik-exercise
- 
+
+# Eye Exercise
+
+![HTML](https://img.shields.io/badge/html-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS](https://img.shields.io/badge/css-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+This repo contains the Formik form exercise from my MIT Full Stack development course.
+
+## Description
+
+A simple login form using the Formik library. This exercise targets a few skills building
+
+- Using the "useFormik" hook
+- Validate input data
+
+## Install
+
+Clone this repo, go to the project folder and run the following from a terminal window:
+Install dependency
+
+```
+npm install
+```
+
+Run project
+
+```
+npm start
+```
+
+## Licensing
+
+This project is under MIT license.
--- src/App.js
+++ src/App.js
@@ -7,43 +7,82 @@ function App() {
       email: "",
       password: "",
     },
-    onSubmit: (values) => {
+    onSubmit: (values, { resetForm }) => {
       console.log("Form: ", values);
+      alert(`Login Successful! \nWelcome ${values.email}`);
+      resetForm({ values: "" });
     },
     validate: (values) => {
       let errors = {};
-      if (!values.email) errors.email = "Field required";
+      if (!values.email) {
+        errors.email = "Field required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Username should be an email";
+      }
       if (!values.password) errors.password = "Field required";
       return errors;
     },
   });
 
   return (
-    <div className="app">
-      <form onSubmit={formik.handleSubmit}>
+    <div className="app d-flex  align-items-center justify-content-center  mt-5">
+      <div
+        className="d-flex flex-column border rounded-3 p-3 shadow"
+        style={{ width: "25rem" }}
+      >
+        <h2 className="text-center">Login</h2>
+        <form onSubmit={formik.handleSubmit}>
+          <div>Email</div>
+          <input
+            id="emailField"
+            name="email"
+            type="text"
+            className="form-control"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
 
-        <div>Email</div>
-        <input id="emailField" name="email" type="text" onChange={formik.handleChange} value={formik.values.email} />
-        <div> {formik.errors.email ? (
-          <span style={{ color: "red" }}>
-            {formik.errors.email}
-          </span>
-        ) : null}</div>
-        <div>
-          Pasword{" "}
-          {formik.errors.password ? (
-            <span style={{ color: "red" }}>
-              {formik.errors.password}
-            </span>
+          {formik.errors.email ? (
+            <div
+              id="emailError"
+              style={{ color: "red" }}
+            >
+              {formik.errors.email}
+            </div>
           ) : null}
-        </div>
-        <input id="pswField" name="password" type="text" onChange={formik.handleChange} value={formik.values.password} />
 
-        <div>
-          <br />
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+          <div>Password</div>
+          <input
+            id="pswField"
+            name="password"
+            type="password"
+            className="form-control"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+
+          {formik.errors.password ? (
+            <div
+              id="pswError"
+              style={{ color: "red" }}
+            >
+              {formik.errors.password}
+            </div>
+          ) : null}
+
+          <div>
+            <button
+              id="submitBtn"
+              className="form-control btn btn-primary btn-block mt-3"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
